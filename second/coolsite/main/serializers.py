b2674ae53627884d44eed52d13496e8e54bdbdf2
StdCoutPy <110192059+StdCoutPy@users.noreply.github.com>
2026-02-25 23:05:29 +0300

    Delete second directory

--- second/coolsite/main/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import P_Menu
-
-class ShopSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = P_Menu
-        fields = ('title',)
--- second/coolsite/main/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-from .models import  P_Menu
-
-
-
-@register(P_Menu)
-class P_MenuTranslationOptions(TranslationOptions):
-    fields = ('title' , 'text')
